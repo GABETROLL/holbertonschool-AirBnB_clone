bd70367466469deecf4010b978d121f84d79d3ea
ntpadovani <normant_padovani@hotmail.com>
2023-02-22 09:27:07 -0400

    Updated console.py per task 7 of the project

--- console.py
+++ console.py
@@ -12,15 +12,15 @@ from models.base_model import BaseModel
 # from models.place import Place
 # from models.review import Review
 # from models.state import State
-# from models.user import User
+from models.user import User
 from models.engine.file_storage import FileStorage
 from models import storage
 import shlex
 
 
-#classGroup = {"Amenity": Amenity, "BaseModel": BaseModel,
-#              "City": City, "Place": Place, "Review": Review,
-#              "State": State, "User": User}
+classGroup = {"Amenity": Amenity, "BaseModel": BaseModel,
+              "City": City, "Place": Place, "Review": Review,
+              "State": State, "User": User}
 
 
 class HBNBCommand(cmd.Cmd):
